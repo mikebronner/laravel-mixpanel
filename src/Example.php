5435b76db648197e40f587abb0f16ee905da5458
Mike Bronner <mike@genealabs.com>
2015-05-30 07:43:00 -0700

    Initial commit

---
+++ src/Example.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace GeneaLabs\MixPanel;
+
+class Example
+{
+
+}
---
+++ tests/ExampleTest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace GeneaLabs\MixPanel\Tests;
+
+class ExampleTest extends \PHPUnit_Framework_TestCase
+{
+
+}
