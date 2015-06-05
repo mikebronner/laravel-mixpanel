40f4abf84adb9cb0c71039c01010511fddd938e4
Mike Bronner <mike@genealabs.com>
2015-06-04 20:44:43 -0700

    Fix parsing of user id

--- src/HTTP/Controllers/StripeWebhooksController.php
+++ src/HTTP/Controllers/StripeWebhooksController.php
@@ -22,7 +22,7 @@ class StripeWebhooksController extends Controller
 
         $transaction = $data['data']['object'];
         $originalValues = (array_key_exists('previous_attributes', $data['data']) ? $data['data']['previous_attributes'] : []);
-        $stripeCustomerId = array_key_exists('customer', $transaction) ? $transaction['customer'] : $transaction['subscriptions']['data'][0]['customer'];
+        $stripeCustomerId = array_key_exists('customer', $transaction) ? $transaction['customer'] : isset($transaction['subscriptions']['data'][0]['customer']) ?: null;
         $user = App::make(config('auth.model'))->where('stripe_id', $stripeCustomerId)->first();
 
         if (! $user) {
