acc39410846fbc5bf9da42edde2f288b0e70f07e
Mike Bronner <mike@genealabs.com>
2015-06-16 22:56:54 -0700

    Use eventhandler to track page views, as other methods were firing multiple times per view

--- src/MixPanelServiceProvider.php
+++ src/MixPanelServiceProvider.php
@@ -14,9 +14,8 @@ class MixPanelServiceProvider extends ServiceProvider
         include __DIR__ . '/Http/routes.php';
 
         $this->app->make(config('auth.model'))->observe(new MixPanelUserObserver($mixPanel, $request));
-        $eventHandler = new MixPanelUserEventHandler($guard, $mixPanel, $request);
+        $eventHandler = new MixPanelEventHandler($guard, $mixPanel, $request);
 
-        view()->composer('*', MixPanelViewComposer::class);
         Event::subscribe($eventHandler);
     }
 
