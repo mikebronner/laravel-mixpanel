b66561a8296c3bd85be293ca2ab57868a541a8de
Mike Bronner <mike@genealabs.com>
2015-08-23 20:00:11 -0700

    Set connection timouts to 2 seconds to prevent site from hanging

--- src/MixPanel.php
+++ src/MixPanel.php
@@ -4,11 +4,18 @@ use Illuminate\Support\Facades\App;
 
 class MixPanel extends \Mixpanel
 {
+    private $defaults = [
+        'connect_timeout' => 2,
+        'timeout' => 2,
+    ];
+
     /**
      * @param array $options
      */
     public function __construct(array $options = [])
     {
+        $options = array_merge($this->defaults, $options);
+
         parent::__construct(config('services.mixpanel.token'), $options);
     }
 }
