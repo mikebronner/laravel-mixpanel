ca63617d91d7bc34c4a40ec551d9fbc98e5678f7
Mike Bronner <mike@genealabs.com>
2017-03-01 08:02:02 -0800

    WIP

--- resources/views/partials/mixpanel.blade.php
+++ resources/views/partials/mixpanel.blade.php
@@ -1,4 +1,1 @@
-<script>
-    {!! file_get_contents(public_path('genealabs-laravel-mixpanel/js/mixpanel.js')) !!}
-    mixpanel.init("{{ config('services.mixpanel.token')}}");
-</script>
+{!! file_get_contents(public_path('genealabs-laravel-mixpanel/js/mixpanel.js')) !!}
