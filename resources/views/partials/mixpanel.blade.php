2947377f82d32035426bfafe317d11c544299c7c
Mike Bronner <mike@genealabs.com>
2017-04-18 15:21:57 -0700

    Update MixPanel JS functionality

--- resources/views/partials/mixpanel.blade.php
+++ resources/views/partials/mixpanel.blade.php
@@ -1,3 +1,4 @@
 <script>
 {!! file_get_contents(public_path('genealabs-laravel-mixpanel/js/mixpanel.js')) !!}
+mixpanel.init("{{ config('services.mixpanel.token') }}",  {api_host: "https://api.mixpanel.com"});
 </script>
