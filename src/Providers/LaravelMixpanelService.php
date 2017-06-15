d673ddca5a4433edf9f3d2a371de679cd9f8fa28
Mike Bronner <mike@genealabs.com>
2017-06-15 08:35:49 -0700

    Fix instanciation of service providers for either L5.1 or 5.3+

--- src/Providers/LaravelMixpanelService.php
+++ src/Providers/LaravelMixpanelService.php
@@ -1,56 +1,25 @@
 <?php namespace GeneaLabs\LaravelMixpanel\Providers;
 
-use GeneaLabs\LaravelMixpanel\LaravelMixpanel;
-use GeneaLabs\LaravelMixpanel\Listeners\LaravelMixpanelEventHandler;
-use GeneaLabs\LaravelMixpanel\Listeners\LaravelMixpanelUserObserver;
-use GeneaLabs\LaravelMixpanel\Console\Commands\Publish;
-use GeneaLabs\LaravelMixpanel\Events\MixpanelEvent;
-use GeneaLabs\LaravelMixpanel\Listeners\MixpanelEvent as MixpanelEventListener;
-use Illuminate\Contracts\View\View;
-use Illuminate\HTTP\Request;
-use Illuminate\Support\ServiceProvider;
-use Illuminate\Foundation\Support\Providers\EventServiceProvider;
 use Illuminate\Contracts\Events\Dispatcher as DispatcherContract;
 
-class LaravelMixpanelService extends EventServiceProvider
-{
-    protected $defer = false;
-    protected $listen = [
-        MixpanelEvent::class => [
-            MixpanelEventListener::class,
-        ],
-    ];
-
-    public function boot(DispatcherContract $events)
+if (starts_with(app()->version(), '5.1.')) {
+    class LaravelMixpanelService extends MixpanelBaseService
     {
-        parent::boot($events);
+        public function boot(DispatcherContract $events)
+        {
+            parent::boot($events);
 
-        include __DIR__ . '/../../routes/api.php';
-
-        $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'genealabs-laravel-mixpanel');
-        $this->publishes([
-            __DIR__ . '/../../public' => public_path(),
-        ], 'assets');
-
-        if (config('services.mixpanel.enable-default-tracking')) {
-            $authModel = config('auth.providers.users.model') ?? config('auth.model');
-            $this->app->make($authModel)->observe(new LaravelMixpanelUserObserver());
-            app('events')->subscribe(new LaravelMixpanelEventHandler());
+            $this->initialize();
         }
     }
-
-    public function register()
+} else {
+    class LaravelMixpanelService extends MixpanelBaseService
     {
-        $this->mergeConfigFrom(__DIR__ . '/../../config/services.php', 'services');
-        $this->commands(Publish::class);
-        $this->app->singleton('mixpanel', LaravelMixpanel::class);
-    }
+        public function boot()
+        {
+            parent::boot();
 
-    /**
-     * @return array
-     */
-    public function provides()
-    {
-        return ['genealabs-laravel-mixpanel'];
+            $this->initialize();
+        }
     }
 }
---
+++ src/Providers/MixpanelBaseService.php
@@ -0,0 +1,53 @@
+<?php namespace GeneaLabs\LaravelMixpanel\Providers;
+
+use GeneaLabs\LaravelMixpanel\LaravelMixpanel;
+use GeneaLabs\LaravelMixpanel\Listeners\LaravelMixpanelEventHandler;
+use GeneaLabs\LaravelMixpanel\Listeners\LaravelMixpanelUserObserver;
+use GeneaLabs\LaravelMixpanel\Console\Commands\Publish;
+use GeneaLabs\LaravelMixpanel\Events\MixpanelEvent;
+use GeneaLabs\LaravelMixpanel\Listeners\MixpanelEvent as MixpanelEventListener;
+use Illuminate\Contracts\View\View;
+use Illuminate\HTTP\Request;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider;
+
+abstract class MixpanelBaseService extends EventServiceProvider
+{
+    protected $defer = false;
+    protected $listen = [
+        MixpanelEvent::class => [
+            MixpanelEventListener::class,
+        ],
+    ];
+
+    protected function initialize()
+    {
+        include __DIR__ . '/../../routes/api.php';
+
+        $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'genealabs-laravel-mixpanel');
+        $this->publishes([
+            __DIR__ . '/../../public' => public_path(),
+        ], 'assets');
+
+        if (config('services.mixpanel.enable-default-tracking')) {
+            $authModel = config('auth.providers.users.model') ?? config('auth.model');
+            $this->app->make($authModel)->observe(new LaravelMixpanelUserObserver());
+            app('events')->subscribe(new LaravelMixpanelEventHandler());
+        }
+    }
+
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../../config/services.php', 'services');
+        $this->commands(Publish::class);
+        $this->app->singleton('mixpanel', LaravelMixpanel::class);
+    }
+
+    /**
+     * @return array
+     */
+    public function provides()
+    {
+        return ['genealabs-laravel-mixpanel'];
+    }
+}
