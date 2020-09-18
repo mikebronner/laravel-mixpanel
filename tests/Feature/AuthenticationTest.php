921517d5197d3616d7e4eeddce96fa5d73bec368
Aaron J. Rubin <aaron@arkitech.net>
2020-09-17 20:13:59 -0400

    Adds the ability to define a getMixPanelKey method on the user class to use for the identifier

--- tests/Feature/AuthenticationTest.php
+++ tests/Feature/AuthenticationTest.php
@@ -1,9 +1,11 @@
 <?php namespace GeneaLabs\LaravelMixpanel\Tests\Feature;
 
 use App\User;
+use GeneaLabs\LaravelMixpanel\Facades\Mixpanel;
 use GeneaLabs\LaravelMixpanel\Tests\FeatureTestCase;
 use GeneaLabs\LaravelMixpanel\Listeners\LoginAttempt;
 use GeneaLabs\LaravelMixpanel\Events\MixpanelEvent;
+use GeneaLabs\LaravelMixpanel\Tests\Fixtures\App\UserWithMixKey;
 use Illuminate\Auth\Events\Attempting;
 use Illuminate\Auth\Events\Login;
 use Illuminate\Auth\Events\Logout;
@@ -65,5 +67,33 @@ class AuthenticationTest extends FeatureTestCase
         Event::assertDispatched(MixpanelEvent::class, function ($event) use ($user) {
             return ($event->user->email === $user->email && $event->names()->contains('User Logged Out'));
         });
+    }
+
+    public function testUsingAlternateKey()
+    {
+        $spy = Mixpanel::spy();
+        $spy->people = new \Producers_MixpanelPeople([]);
+
+        $user = UserWithMixKey::create([
+            'name'     => 'Test User',
+            'email'    => 'tester@testing.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        event(new MixpanelEvent($user, ['User Logged In' => []]));
+
+        $spy->shouldHaveReceived('identify')->with('random-other-identifier');
+    }
+
+    public function testUsingDefaultKey()
+    {
+        $spy = Mixpanel::spy();
+        $spy->people = new \Producers_MixpanelPeople([]);
+
+        $user = factory(User::class)->create();
+
+        event(new MixpanelEvent($user, ['User Logged In' => []]));
+
+        $spy->shouldHaveReceived('identify')->with($user->id);
     }
 }
